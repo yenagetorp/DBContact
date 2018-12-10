fd58b9de9d4a62130130f1affc6bbd792847a2a3
Yen Agetorp <yen.agetorp@gmail.com>
2018-12-10 15:28:10 +0100

    Add project files.

---
+++ DBContactTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBContactTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
